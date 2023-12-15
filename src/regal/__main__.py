e904f2c16e2c76852bce86c556e7bf981a34ae7b
Coleman A. Matey <113218526+colemanmatey@users.noreply.github.com>
2023-12-15 13:39:09 +0000

    Update __main__.py

--- src/regal/__main__.py
+++ src/regal/__main__.py
@@ -2,12 +2,25 @@
 Main
 """
 
-from regal.config import ConfigManager
+from regal.database import DatabaseManager
 
 
 def main():
-    config = ConfigManager(".env")
-    
+    db = DatabaseManager()
+    db.connect()
+
+    query = "SELECT * FROM studentTbl"
+    result = db.fetch_data(query)
+
+    students = []
+
+    for record in result:
+        students.append(f"{record[3]} {record[4]}")
+
+    for i in students:
+        print(i)
+
+    db.disconnect()
 
 if __name__ == '__main__':
     main()
